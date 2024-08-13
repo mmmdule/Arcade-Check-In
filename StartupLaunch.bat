a3035a7ec4abf663d0f38ab84438d8fe9d8e5665
mmmdule <113645355+mmmdule@users.noreply.github.com>
2024-08-13 22:11:57 +0200

    vbox path to .ps1 file

--- StartupLaunch.bat
+++ StartupLaunch.bat
@@ -1,1 +1,1 @@
-PowerShell.exe -WindowStyle hidden -File "D:\Code\Ilija PowerShell igraonica\StartupLaunch.ps1"
+PowerShell.exe -WindowStyle hidden -File "C:\Code\1\Arcade-Check-In-main\Fullscreen Form Arcade.ps1"
